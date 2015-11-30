ffb02a738da70c9e89c7cfaf039aeb690add6732
Ganesh Ravichandran <gr2483@columbia.edu>
2015-11-29 19:27:01 -0500

    packery.js and draggabilly working

--- static/js/main2.js
+++ static/js/main2.js
@@ -1,24 +1,28 @@
 var $container = $('#grid-wrapper').packery({
-	columnWidth: '#likelihood-plot-pl-panel',
-	rowHeight: '#likelihood-plot-pl-panel',
+	columnWidth: 20,
+	rowHeight: 20,
 });
 
-$container.find('.draggable').each( function( i, itemElem ) {
+$container.find('.draggable').each(makeEachDraggable);
+
+function makeEachDraggable(i, itemElem) {
 	// make element draggable with Draggabilly
-	console.log($.type(itemElem));
 	var draggie = new Draggabilly( itemElem );
 	// bind Draggabilly events to Packery
 	$container.packery( 'bindDraggabillyEvents', draggie );
-});
+}
 
-function removeFromGrid(liObj) {
-	$divObj = $('#' + liObj.id + '-panel');
-
-	if ($divObj.length){
-		$container.packery('remove', $divObj);
+function packeryToggle(liObj, extraClass, imgName) {
+	objID = liObj.id + '-panel';
+	if ($('#' + objID).length){
+		$container.packery('remove', $('#' + objID));
 	}
 	else {
-		$container.packery('addItems', gg);
+		$addObj = $('<div id="' + objID + '" class="' + extraClass + ' draggable">\
+						<img src="http://tigress-web.princeton.edu/~tmorton/koi-fpp/K00087.01/' + imgName + '.png">\
+					</div>');
+		$container.append($addObj).packery('appended', $addObj);
+		$addObj.each(makeEachDraggable);
 	}
 	$container.packery();
 }
@@ -32,61 +36,53 @@ $('#submit').click( function() {
 });
 
 $('#fpp-summary').click( function() {
-	$('#fpp-summary-panel').toggle();
+	packeryToggle(this, '', 'FPPsummary');
 });
 
 $('#dartmouth-physical-single-star').click( function() {
-
-	$divObj = $('#dartmouth-physical-single-star-panel');
-
-	if ($divObj.length) {
-		$container.packery('remove', $divObj);
-	}
-	else {
-		gg = $('<div style="border: 5px solid #000" id="dartmouth-physical-single-star-panel" class="dartmouth draggable">\
-					<img src="http://tigress-web.princeton.edu/~tmorton/koi-fpp/K00087.01/dartmouth_triangle_single_physical.png">\
-				</div>');
-		$container.packery('addItems', gg);
-	}
-	$container.packery();
+	packeryToggle(this, 'dartmouth', 'dartmouth_triangle_single_physical');
 });
 
 $("#dartmouth-physical-binary-star").click( function() {
-	$("#dartmouth-physical-binary-star-panel").toggle();
+	packeryToggle(this, 'dartmouth', 'dartmouth_triangle_binary_physical');
 });
 
 $("#dartmouth-physical-triple-star").click( function() {
-	$("#dartmouth-physical-triple-star-panel").toggle();
+	packeryToggle(this, 'dartmouth', 'dartmouth_triangle_triple_physical');
 });
 
 $("#dartmouth-observed-single-star").click( function() {
-	$("#dartmouth-observed-single-star-panel").toggle();
+	packeryToggle(this, 'dartmouth', 'dartmouth_triangle_single_observed');
 });
 
 $("#dartmouth-observed-binary-star").click( function() {
-	$("#dartmouth-observed-binary-star-panel").toggle();
+	packeryToggle(this, 'dartmouth', 'dartmouth_triangle_binary_observed');
 });
 
 $("#dartmouth-observed-triple-star").click( function() {
-	$("#dartmouth-observed-triple-star-panel").toggle();
+	packeryToggle(this, 'dartmouth', 'dartmouth_triangle_triple_observed');
 });
 
 $("#likelihood-plot-pl").click( function() {
-	$("#likelihood-plot-pl-panel").toggle();
+	packeryToggle(this, 'likelihood-plot', 'pl');
 });
 
 $("#likelihood-plot-eb").click( function() {
-	$("#likelihood-plot-eb-panel").toggle();
+	packeryToggle(this, 'likelihood-plot', 'eb');
 });
 
 $("#likelihood-plot-beb").click( function() {
-	$("#likelihood-plot-beb-panel").toggle();
+	packeryToggle(this, 'likelihood-plot', 'beb');
 });
 
 $("#likelihood-plot-heb").click( function() {
-	$("#likelihood-plot-heb-panel").toggle();
+	packeryToggle(this, 'likelihood-plot', 'heb');
 });
 
 $("#signal").click( function() {
-	$("#signal-panel").toggle();
+	packeryToggle(this, 'signal-plot', 'signal');
+});
+
+$('#control-panel>ul>li').click( function(){
+	$(this).toggleClass('selected');
 });
